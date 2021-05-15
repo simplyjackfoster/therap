bc643a9dac80ecdd22c51a208cd044369598322c
Jack Foster <jackfoster246@gmail.com>
2021-05-15 00:27:00 -0400

    Matamo

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1933,6 +1933,21 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap requires jQuery'
 
   // AFFIX DATA-API
   // ==============
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://teresafostertherapy.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/teresafostertherapy.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 
   $(window).on('load', function () {
     $('[data-spy="affix"]').each(function () {
